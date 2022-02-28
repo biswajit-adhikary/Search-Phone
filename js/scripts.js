e68ea5ea7f1dce9e6b6ef9e36e487a46ffb3da06
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-03-01 01:37:24 +0600

    Display data error handling done

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,35 @@ const getPhone = () => {
     const searchValue = searchInput.value;
     searchInput.value = '';
 
-    // Get Data From API
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhone(data.data));
+    // Error handling & get data
+    const errorDiv = document.getElementById('error');
+    const mainDiv = document.getElementById('main-div');
+    if (searchValue == '') {
+        errorDiv.innerHTML = `<p class="text-danger">Please write Phone name!</p>`;
+        mainDiv.innerHTML = '';
+    } else {
+        // Get Data From API
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhone(data.data));
+    }
 }
 
 // Function for display data
 const displayPhone = phones => {
     const mainDiv = document.getElementById('main-div');
-    // Reset value
     mainDiv.textContent = '';
-    // Loop and show data
-    phones.forEach(phone => {
-        const newCol = document.createElement('div');
-        newCol.classList.add('col-lg-4');
-        newCol.innerHTML = `
+    // Error handling & display data
+    const errorDiv = document.getElementById('error');
+    if (phones.length == 0) {
+        errorDiv.innerHTML = `<p class="text-danger">No phone found!</p>`;
+    } else {
+        errorDiv.innerHTML = ``;
+        phones.forEach(phone => {
+            const newCol = document.createElement('div');
+            newCol.classList.add('col-lg-4');
+            newCol.innerHTML = `
             <div class="card mb-4 text-center">
                 <div class="card-image">
                     <img src="${phone.image}" class="card-img-top" alt="">
@@ -33,6 +45,8 @@ const displayPhone = phones => {
                 </div>
             </div>
         `;
-        mainDiv.appendChild(newCol);
-    })
+            mainDiv.appendChild(newCol);
+        })
+    }
+
 }
