a290757d675446cabdf48d37894ce9624e47971c
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-03-01 01:18:08 +0600

    Display Data Done

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Function for get data
 const getPhone = () => {
     // Get & Reset Search Value
     const searchInput = document.getElementById('form-search');
@@ -11,6 +12,27 @@ const getPhone = () => {
         .then(data => displayPhone(data.data));
 }
 
+// Function for display data
 const displayPhone = phones => {
-    console.log(phones);
+    const mainDiv = document.getElementById('main-div');
+    // Reset value
+    mainDiv.textContent = '';
+    // Loop and show data
+    phones.forEach(phone => {
+        const newCol = document.createElement('div');
+        newCol.classList.add('col-lg-4');
+        newCol.innerHTML = `
+            <div class="card mb-4 text-center">
+                <div class="card-image">
+                    <img src="${phone.image}" class="card-img-top" alt="">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand}</p>
+                    <button class="btn btn-primary">Details</button>
+                </div>
+            </div>
+        `;
+        mainDiv.appendChild(newCol);
+    })
 }
